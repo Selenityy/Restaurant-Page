e455e22204b3593eb8a60879d9ad8c9ba195639e
Selena <selena.k.rodriguez@gmail.com>
2023-03-14 18:24:01 -0500

    Added first onclick eventlistener to the menu text

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import homePage from "./pages/home/homePage";
+import menuPage from "./pages/menu/menuPage";
+import { removeChildrenNodes } from "./functions/pageOnLoad";
 import "./styles/home.css";
+import "./styles/menu.css";
 
 homePage();
+
+const menuButton = document.getElementById("menuNavBtn");
+
+menuButton.addEventListener("click", function () {
+  removeChildrenNodes();
+  menuPage();
+});
