cc91ad740d0d42aacce8ed3ad491fd1c2d923b3e
Selena <selena.k.rodriguez@gmail.com>
2023-03-14 19:13:10 -0500

    Saved

--- src/pages/home/header.js
+++ src/pages/home/header.js
@@ -10,30 +10,27 @@ export default function homeNavBar() {
   header.appendChild(headerNavLinks);
 
   const homeNavBtn = document.createElement("a");
+  //homeNavBtn.addEventListener("click", func);
   homeNavBtn.classList.add("headerNavBtn");
   homeNavBtn.setAttribute("id", "homeNavBtn");
-  // homeNavBtn.value = "home";
   homeNavBtn.textContent = "Home";
   headerNavLinks.appendChild(homeNavBtn);
 
   const menuNavBtn = document.createElement("a");
   menuNavBtn.classList.add("headerNavBtn");
   menuNavBtn.setAttribute("id", "menuNavBtn");
-  // menuNavBtn.value = "menu";
   menuNavBtn.textContent = "Menu";
   headerNavLinks.appendChild(menuNavBtn);
 
   const aboutNavBtn = document.createElement("a");
   aboutNavBtn.classList.add("headerNavBtn");
   aboutNavBtn.setAttribute("id", "aboutNavBtn");
-  // aboutNavBtn.value = "about";
   aboutNavBtn.textContent = "About";
   headerNavLinks.appendChild(aboutNavBtn);
 
   const contactNavBtn = document.createElement("a");
   contactNavBtn.classList.add("headerNavBtn");
   contactNavBtn.setAttribute("id", "contactNavBtn");
-  // contactNavBtn.value = "contact";
   contactNavBtn.textContent = "Contact";
   headerNavLinks.appendChild(contactNavBtn);
 }
