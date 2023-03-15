d68638c2a53f8ab81169f96367329ce2b244af11
Selena <selena.k.rodriguez@gmail.com>
2023-03-14 19:12:50 -0500

    Created parent and children divs for each piece of content

--- src/pages/home/body.js
+++ src/pages/home/body.js
@@ -1,7 +1,46 @@
-export default function homeBody() {
-    const content = document.getElementById("content");
+import totoroLogoImg from "../../assets/totoro-logo.png"
 
-    const body = document.createElement("div");
-    body.setAttribute("id", "bodyContent");
-    content.appendChild(body);
+export default function homeBody() {
+  const content = document.getElementById("content");
+
+  const body = document.createElement("div");
+  body.setAttribute("id", "bodyContent");
+  content.appendChild(body);
+
+  const cafeBranding = document.createElement("div");
+  cafeBranding.setAttribute("id", "cafeBranding");
+  cafeBranding.classList.add("bodyClass");
+  body.appendChild(cafeBranding);
+
+  const totoroTitle = document.createElement("div");
+  totoroTitle.setAttribute("id", "totoroTitle");
+  totoroTitle.textContent = "Totoro Cafe";
+  cafeBranding.appendChild(totoroTitle);
+
+  const totoroLogo = document.createElement("img");
+  totoroLogo.setAttribute("id", "totoroLogo");
+  totoroLogo.src = totoroLogoImg;
+  cafeBranding.appendChild(totoroLogo);
+
+  const cafeDescription = document.createElement("div");
+  cafeDescription.setAttribute("id", "cafeDescription");
+  cafeDescription.classList.add("bodyClass");
+  body.appendChild(cafeDescription);
+
+  const lorem = document.createElement("div");
+  lorem.setAttribute("id", "lorem");
+  lorem.textContent =
+    "Lorem ipsum dolor sit amet. Nam excepturi officia ut impedit tempora At autem minus non mollitia corrupti! Est placeat quas aut laboriosam blanditiis et rerum excepturi? Et fugit fuga qui sunt exercitationem non animi repudiandae aut repellendus internos et explicabo dolorem eos dolorum veritatis ut doloremque molestiae. Ea internos atque cum tempore amet aut aliquid internos non ipsa enim.";
+  cafeDescription.appendChild(lorem);
+
+  const homePageMenuBtn = document.createElement("div");
+  homePageMenuBtn.setAttribute("id", "homePageMenuBtn");
+  homePageMenuBtn.classList.add("bodyClass");
+  body.appendChild(homePageMenuBtn);
+
+  const menuBtn = document.createElement("button");
+  menuBtn.setAttribute("id", "menuBtn");
+  menuBtn.textContent = "Menu"
+  homePageMenuBtn.appendChild(menuBtn);
+
 }
