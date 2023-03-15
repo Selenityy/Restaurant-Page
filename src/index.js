d7204f702961fc403f5d2238e6c4e6c5f9a90234
Selena <selena.k.rodriguez@gmail.com>
2023-03-15 15:19:29 -0500

    Added the allChildren div here to not have repetition, commented the code

--- src/index.js
+++ src/index.js
@@ -1,20 +1,29 @@
 import homePage from "./pages/home/homePage";
 import menuPage from "./pages/menu/menuPage";
-import { removeChildrenNodes } from "./functions/pageOnLoad";
+import { removeChildrenNodes } from "./functions/pageFuncs";
 import "./styles/home.css";
 import "./styles/menu.css";
 
+// sets up the divs for the pages
+const content = document.getElementById("content");
+const allChildren = document.createElement("div");
+allChildren.setAttribute("id", "allChildren");
+content.appendChild(allChildren);
+
+// runs the home page
 homePage();
 
 const menuButton = document.getElementById("menuNavBtn");
 const bodyMenuButton = document.getElementById("menuBtn");
 
+// header menu button listens for click
 menuButton.addEventListener("click", function () {
   removeChildrenNodes();
   menuPage();
 });
 
+// body text button listens for click
 bodyMenuButton.addEventListener("click", function () {
-    removeChildrenNodes();
-    menuPage();
-  });
+  removeChildrenNodes();
+  menuPage();
+});
